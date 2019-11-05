092e55edb74646fb06c090132ba2560f29f82945
SergejGorev <s.gorev1985@gmail.com>
2019-11-06 00:05:44 +0100

    first try

---
+++ get_data.py
@@ -0,0 +1,18 @@
+import quandl
+
+
+#todo dictionary with markets as input
+
+#some change ad
+
+#todo get COT and get Price for each market
+
+#todo save data into files as CSV
+
+#todo check the last row in files to see if the data is uptodate
+
+#todo if not uptodate download data, for specific markets
+
+#todo check when Quandl updates the data, in order to hard code when to update
+
+#todo how to structure that script, classes or just functions
