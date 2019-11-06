bdacb28537337928c1a913fbb141302acf2d0dc9
SergejGorev <s.gorev1985@gmail.com>
2019-11-06 20:16:56 +0100

    Ready with get COT. todo get market data

--- get_data.py
+++ get_data.py
@@ -158,6 +158,5 @@ def write_into_file():
         print('Done')
 
 write_into_file()
-#todo check the last row in files to see if the data is uptodate
-#todo if not uptodate download data, for specific markets
+
 #todo check when Quandl updates the data, in order to hard code when to update
